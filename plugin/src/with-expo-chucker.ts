a7564637e1be7f8a7f403ade23e2afd773f4d79b
Reroet <wayanpuguhsudarma@gmail.com>
2023-11-14 12:56:31 +0800

    feat: add CHUCKER_ENABLED into gradle properties to conditional install chucker no-op

--- plugin/src/with-expo-chucker.ts
+++ plugin/src/with-expo-chucker.ts
@@ -2,30 +2,29 @@ import {
   AndroidConfig,
   ConfigPlugin,
   withAndroidManifest,
+  withGradleProperties,
   withPlugins,
 } from "@expo/config-plugins";
 import { addPermission } from "@expo/config-plugins/build/android/Permissions";
 
 // add flag enabled/disabled to activate chucker at dev discretion
-const withFlagMetadataMainApplication: ConfigPlugin<{ enabled?: boolean }> = (
+const withFlagMetadataMainApplication: ConfigPlugin<{ enabled: boolean }> = (
   config,
   { enabled }
 ) => {
-  return withAndroidManifest(config, async (config) => {
-    const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(
-      config.modResults
-    );
+  // adding CHUCKER_ENABLED flag to gradle.properties
+  const gradleProperties = withGradleProperties(config, (gradleConfig) => {
+    gradleConfig.modResults =
+      AndroidConfig.BuildProperties.updateAndroidBuildProperty(
+        gradleConfig.modResults,
+        "CHUCKER_ENABLED",
+        enabled ? "true" : "false"
+      );
 
-    const isEnabled = typeof enabled === "undefined" ? true : enabled;
-
-    AndroidConfig.Manifest.addMetaDataItemToMainApplication(
-      mainApplication,
-      "CHUCKER_ENABLED",
-      isEnabled ? "true" : "false"
-    );
-
-    return config;
+    return gradleConfig;
   });
+
+  return gradleProperties;
 };
 
 const withPermission: ConfigPlugin = (config) => {
