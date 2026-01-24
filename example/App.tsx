633a1d8446dee8eb0a2e1a6df08be0d67207ebb6
puguh <wayanpuguhsudarma@gmail.com>
2026-01-24 15:52:35 +0800

    [Upgrade]: Expo 52 (#8)
    * chore: adding prettier config
    
    * chore: cleanup code
    
    * chore: Migrate to pnpm, update Expo configuration to app.json, and upgrade dependencies and example app to latest Expo SDK.
    
    * feat: integrate expo-notifications and enhance example app with a request button, loading indicator, and `useTransition` hook.

---
+++ example/App.tsx
@@ -0,0 +1,80 @@
+import * as Notifications from 'expo-notifications';
+import { useEffect, useState, useTransition } from "react";
+import { ActivityIndicator, Button, FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    requestPermissionsAsync();
+  }, []);
+
+  function handlePress() {
+    startTransition(() => {
+      getGithubUsers().then((users) => setUsers(users));
+    });
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="Do HTTP Request"
+        onPress={handlePress}
+        disabled={isPending}
+      />
+      {isPending && <ActivityIndicator />}
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <Text selectable style={[styles.text, { marginBottom: 8 }]}>
+            {item.login}
+          </Text>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    gap: 16,
+    paddingHorizontal: 16,
+  },
+  text: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+    fontSize: 14,
+    textAlign: "left",
+  },
+});
+
+interface User {
+  id: number;
+  login: string;
+  avatar_url: string;
+  url: string;
+}
+
+async function getGithubUsers(): Promise<User[]> {
+  try {
+    const response = await fetch("https://api.github.com/users");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch users");
+    }
+
+    return response.json() as Promise<User[]>;
+  } catch (e) {
+    console.error(e);
+    console.log(JSON.stringify(e, null, 2));
+    return [];
+  }
+}
+
+function requestPermissionsAsync() {
+  return Notifications.requestPermissionsAsync();
+}
